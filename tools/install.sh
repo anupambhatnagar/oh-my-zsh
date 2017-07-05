ab3c90eb4829e08feeb9ded0e787308f3bb0f341
Anupam Bhatnagar <anupambhatnagar@gmail.com>
2017-07-05 01:04:34 +0000

    removing chsh from install.sh file

--- tools/install.sh
+++ tools/install.sh
@@ -81,18 +81,18 @@ main() {
   mv -f ~/.zshrc-omztemp ~/.zshrc
 
   # If this user's login shell is not already "zsh", attempt to switch.
-  TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
-  if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+  #TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+  #if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
     # If this platform provides a "chsh" command (not Cygwin), do it, man!
-    if hash chsh >/dev/null 2>&1; then
-      printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
-      chsh -s $(grep /zsh$ /etc/shells | tail -1)
+    #if hash chsh >/dev/null 2>&1; then
+     # printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
+     # chsh -s $(grep /zsh$ /etc/shells | tail -1)
     # Else, suggest the user do so manually.
-    else
-      printf "I can't change your shell automatically because this system does not have chsh.\n"
-      printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
-    fi
-  fi
+    #else
+     # printf "I can't change your shell automatically because this system does not have chsh.\n"
+  printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
+    #fi
+  #fi
 
   printf "${GREEN}"
   echo '         __                                     __   '
@@ -101,13 +101,6 @@ main() {
   echo '/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '
   echo '\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '
   echo '                        /____/                       ....is now installed!'
-  echo ''
-  echo ''
-  echo 'Please look over the ~/.zshrc file to select plugins, themes, and options.'
-  echo ''
-  echo 'p.s. Follow us at https://twitter.com/ohmyzsh.'
-  echo ''
-  echo 'p.p.s. Get stickers and t-shirts at http://shop.planetargon.com.'
   echo ''
   printf "${NORMAL}"
   env zsh
